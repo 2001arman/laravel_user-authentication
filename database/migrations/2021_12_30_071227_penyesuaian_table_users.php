1ca3988cc371f55ba14f37db4a55059f97c55e95
Arman <2001armanmaulana@gmail.com>
2021-12-30 15:41:09 +0800

    tugas fwb

---
+++ database/migrations/2021_12_30_071227_penyesuaian_table_users.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PenyesuaianTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string("username")->unique();
+            $table->string("roles");
+            $table->string("address");
+            $table->string("phone");
+            $table->string("avatar");
+            $table->enum("status", ["ACTIVE", "INACTIVE"]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropdown("username");
+            $table->dropdown("roles");
+            $table->dropdown("address");
+            $table->dropdown("phone");
+            $table->dropdown("avatar");
+            $table->dropdown("status");
+        });
+    }
+}
